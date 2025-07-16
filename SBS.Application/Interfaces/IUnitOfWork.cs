692ca168a900b05f55b55f164399e6cd477962b1
Ziad Ahmad Abdelrahman <68442256+ziad2292@users.noreply.github.com>
2025-07-16 17:56:01 +0300

    Booking API To be done: conflict validation, controller, update operations

---
+++ SBS.Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using SBS.Application.Interfaces.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBS.Application.Interfaces
+{
+    public interface IUnitOfWork
+	{
+		IBookingRepository Bookings { get; }
+		IBookingSlotRepository BookingSlotRepository { get; }
+
+		Task<int> CommitAsync();
+		Task BeginTransactionAsync();
+		Task CommitTransactionAsync();
+		Task RollBackTransactionAsync();
+	}
+}
---
+++ SBS.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SBS.Application.Interfaces.IRepositories;
+using SBS.Domain.Entities;
+using SBS.Infrastructure.Persistence._Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBS.Infrastructure.Repositories
+{
+	internal abstract class GenericRepository<T> : IRepository<T> where T : class
+	{
+		private readonly AppDbContext _appDbContext;
+
+		public GenericRepository(AppDbContext appDbContext)
+		{
+			_appDbContext = appDbContext;
+		}
+
+		public virtual async Task<List<T>> GetAllAsync()
+		{
+			return await _appDbContext.Set<T>().ToListAsync();
+		}
+
+		public virtual Task UpdateAsync(T instance)
+		{
+			//To do (Make Update Product DTO)
+			return null;
+		}
+
+
+
+
+	}
+}
