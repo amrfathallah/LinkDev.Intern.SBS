7fdf9d372fa57501eaab731f242cfb142b9bded4
AhmedHany231 <113009997+AhmedHany231@users.noreply.github.com>
2025-07-17 10:57:03 +0300

    ResourceDto

---
+++ SBS.Application/DTOs/ResourceDto/CreateResourceDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBS.Application.DTOs.ResourceDto.ResourceDto
+{
+    public class CreateResourceDto
+    {
+        public string Name { get; set; } = default!;
+        public int TypeId { get; set; }
+        public int Capacity { get; set; }
+        public TimeSpan OpenAt { get; set; }
+        public TimeSpan CloseAt { get; set; }
+    }
+}
