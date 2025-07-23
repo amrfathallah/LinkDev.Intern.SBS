854b727b02b2e3de7a1e3e44301ef40577e0285b
Amr Mohamed Fathallah <amrmfathallah11@gmail.com>
2025-07-23 09:23:35 +0300

    remove unused dependencies

--- SmartBooking.Api/ClientApp/src/app/app.module.ts
+++ SmartBooking.Api/ClientApp/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
