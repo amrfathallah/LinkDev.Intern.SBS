c647ac1948794252832174fa0a736f9d37b4fb36
salahmohamed <salahmohamed.sm2016@gmail.com>
2025-07-29 19:52:08 +0300

    feat: add slot management functionality with repository and service layers

--- SBS.Infrastructure/DependencyInjection.cs
+++ SBS.Infrastructure/DependencyInjection.cs
@@ -66,6 +66,9 @@ namespace SBS.Infrastructure
             services.AddScoped<IBookingService, BookingService>();
             services.AddScoped<IResourceService, ResourceService>();
 
+			services.AddScoped<ISlotRepository, SlotRepository>();
+			services.AddScoped<ISlotService, SlotService>();
+
 
             return services;
 		}
--- SmartBooking.Api/ClientApp/src/app/resources/models/dtos/slot.dto.ts
+++ SmartBooking.Api/ClientApp/src/app/resources/models/dtos/slot.dto.ts
@@ -2,6 +2,10 @@ export interface SlotDto {
   id: string;
   startTime: string;
   endTime: string;
+}
+
+// for component's internal slot representation
+export interface ComponentSlotDto extends SlotDto {
   isActive: boolean;
   isBooked: boolean;
 }
